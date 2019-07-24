bb59a412f205b865e645457ca7b6375139cbbc6a
Chad Kirby <ckirby@biddle.com>
2019-07-24 16:54:45 -0700

    Added new sln and projects

---
+++ src/core/Superstring.CoreWrapper/PointWrapper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace Superstring.CoreWrapper
+{
+    public class PointWrapper
+    {
+        [DllImport(@"../cpp/libhello-cpp.so")]
+        public static extern void init();
+
+        [DllImport(@"../cpp/libhello-cpp.so")]
+        public static extern v8::Local<v8::Value> from_point(Point point);
+
+        [DllImport(@"../cpp/libhello-cpp.so")]
+        public static extern optional<Point> point_from_js(v8::Local<v8::Value>);
+
+        [DllImport(@"../cpp/libhello-cpp.so")]
+        extern Point point;
+    }
+}
