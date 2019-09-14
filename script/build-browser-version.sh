e548feafefb3056ff22f8ee429648c88f4c160f3
Chad Kirby <ckirby@biddle.com>
2019-09-14 15:14:25 -0700

    Switching back to em++

--- script/build-browser-version.sh
+++ script/build-browser-version.sh
@@ -36,7 +36,7 @@ emcc                                         \
   vendor/pcre/10.23/src/pcre2_valid_utf.c    \
   vendor/pcre/10.23/src/pcre2_xclass.c
 
-emcc                                                \
+em++                                                \
   -O3                                               \
   -I src/native-src/bindings/em                     \
   -I src/native-src/core                            \
