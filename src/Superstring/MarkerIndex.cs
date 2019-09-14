03c8fa717cc41c127e19cdc85e275e808e347009
Chad Kirby <ckirby@biddle.com>
2019-09-14 12:06:16 -0700

    Working towards WASM load

--- src/Superstring/MarkerIndex.cs
+++ src/Superstring/MarkerIndex.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using WebAssembly;
+using WebAssembly.Runtime;
 
 namespace Superstring
 {
-    public class MarkerIndex
+    public abstract class MarkerIndex
     {
+        private static InstanceCreator<MarkerIndex> _instanceCreator;
+        private static object _lockSync = new { };
+ 
+        public static MarkerIndex Make(uint seed = 0u)
+        {
+            if (_instanceCreator == null)
+            {
+                lock (_lockSync)
+                {
+                    if (_instanceCreator == null)
+                    {
+                        _instanceCreator = Compile.FromBinary<MarkerIndex>("superstring.wasm");
+                    }
+                }
+            }
+
+            using (var instance = _instanceCreator(new ImportDictionary()))
+            {
+                return instance.Exports;
+                var e = new Export();
+            }
+        }
     }
 }
---
+++ src/Superstring/SuperstringCoreExports.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Superstring
+{
+    public abstract class SuperstringCoreExports
+    {
+        //public abstract 
+
+
+
+
+        //void Init(Local<Object> exports)
+        //{
+        //    PointWrapper::init();
+        //    RangeWrapper::init();
+        //    PatchWrapper::init(exports);
+        //    MarkerIndexWrapper::init(exports);
+        //    TextBufferWrapper::init(exports);
+        //    TextWriter::init(exports);
+        //    TextReader::init(exports);
+        //    TextBufferSnapshotWrapper::init();
+        //}
+    }
+}
