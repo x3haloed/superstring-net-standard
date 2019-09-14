b44a0e2a76502163c0ae345b27fbf8afb87b44ad
Chad Kirby <ckirby@biddle.com>
2019-09-14 15:01:25 -0700

    Updated output type of browser build script to wasm

--- script/build-browser-version.sh
+++ script/build-browser-version.sh
@@ -37,8 +37,7 @@ emcc                                         \
   vendor/pcre/10.23/src/pcre2_xclass.c
 
 em++                                                \
-  --bind                                            \
-  -o browser.js                                     \
+  -o superstring.wasm                               \
   -O3                                               \
   -I src/native-src/bindings/em                     \
   -I src/native-src/core                            \
@@ -53,4 +52,3 @@ em++                                                \
   build/pcre.o                                      \
   -s TOTAL_MEMORY=134217728                         \
   --memory-init-file 0                              \
-  "$@"
