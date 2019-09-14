0124d7517790e1d75499bf6f2df132713d3882cc
Chad Kirby <ckirby@biddle.com>
2019-09-14 15:33:40 -0700

    Removed embind from script

--- script/build-browser-version.sh
+++ script/build-browser-version.sh
@@ -37,18 +37,13 @@ emcc                                         \
   vendor/pcre/10.23/src/pcre2_xclass.c
 
 em++                                                \
-  --bind                                            \
   -O3                                               \
-  -I src/native-src/bindings/em                     \
   -I src/native-src/core                            \
   -I vendor/libcxx                                  \
   -I vendor/pcre/include                            \
   -D PCRE2_CODE_UNIT_WIDTH=16                       \
   -xc++                                             \
-  --pre-js src/native-src/bindings/em/prologue.js   \
-  --post-js src/native-src/bindings/em/epilogue.js  \
   src/native-src/core/*.cc                          \
-  src/native-src/bindings/em/*.cc                   \
   build/pcre.o                                      \
   -s TOTAL_MEMORY=134217728                         \
   --memory-init-file 0                              \
